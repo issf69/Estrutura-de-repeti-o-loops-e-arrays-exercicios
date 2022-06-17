ec431f260856ae76314bd8b558286e201563d8d6
Yrene Sylva França <105497075+issf69@users.noreply.github.com>
2022-06-17 15:16:21 -0300

    Update Ex5_Tabuada.java

--- src/dio/com/br/exercicios/loops/Ex5_Tabuada.java
+++ src/dio/com/br/exercicios/loops/Ex5_Tabuada.java
@@ -1,10 +1,52 @@
+###
 package dio.com.br.exercicios.loops;
 
-/**
- *
- */
+import java.util.Scanner;
+
+/*
+Desenvolva um gerador de tabuada,
+capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
+O usuário deve informar de qual numero ele deseja ver a tabuada.
+A saída deve ser conforme o exemplo abaixo:
+
+Tabuada de 5:
+5 X 1 = 5
+5 X 2 = 10s
+...
+5 X 10 = 50
+*/
 public class Ex5_Tabuada {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Tabuada:");
+        int tabuada = scan.nextInt();
+
+        System.out.println("Tabuada de " + tabuada);
+
+        for(int i = 1; i <= 10; i = i + 1) {
+            System.out.println(tabuada + " X " + i + " = " + (tabuada*i));
+
+        }
     }
+
 }
+##
+
+C:\Users\Irene\.jdks\openjdk-18.0.1.1\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.1.1\lib\idea_rt.jar=52134:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.1.1\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\Irene\Documents\Estrutura de repetição loops-e-arrays\loops-e-arrays-xercicios\out\production\loops-e-arrays-xercicios" dio.com.br.exercicios.loops.Ex5_Tabuada
+Tabuada:
+
+9
+Tabuada de 9
+9 X 1 = 9
+9 X 2 = 18
+9 X 3 = 27
+9 X 4 = 36
+9 X 5 = 45
+9 X 6 = 54
+9 X 7 = 63
+9 X 8 = 72
+9 X 9 = 81
+9 X 10 = 90
+
+Process finished with exit code 0
