354647cf50bc3efa2068a5b8e5e77e69d99df609
Yrene Sylva França <105497075+issf69@users.noreply.github.com>
2022-06-17 16:21:26 -0300

    Update Ex6_Fatorial.java

--- src/dio/com/br/exercicios/loops/Ex6_Fatorial.java
+++ src/dio/com/br/exercicios/loops/Ex6_Fatorial.java
@@ -1,10 +1,41 @@
+##
+
 package dio.com.br.exercicios.loops;
 
-/**
- *
- */
+import java.util.Scanner;
+
+/*
+Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+Ex.: 5!= 120 (5 X 4 X 3 X 2 X 1)
+*/
 public class Ex6_Fatorial {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Fatorial: ");
+        int fatorial = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.print(fatorial +"! = ");
+        for(int i = fatorial ;i >= 1 ; i --) {
+            multiplicacao = multiplicacao * i;
+        }
+
+        System.out.println(multiplicacao);
     }
 }
+
+##
+
+C:\Users\Irene\.jdks\openjdk-18.0.1.1\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.1.1\lib\idea_rt.jar=52388:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.1.1\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\Irene\Documents\Estrutura de repetição loops-e-arrays\loops-e-arrays-xercicios\out\production\loops-e-arrays-xercicios" dio.com.br.exercicios.loops.Ex6_Fatorial
+Fatorial: 
+
+5
+5! = 120
+
+Process finished with exit code 0
+
+    ##
+    
+    
